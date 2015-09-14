e7e3d30d675ed544125f5de5e78418513d2f11c2
Matthew Yeung <csyeung87@gmail.com>
2015-09-14 14:53:01 -0400

    more stuff

---
+++ README.md
@@ -0,0 +1,10 @@
+# D3js
+##### D3js is a javascript library for building dynamic, interactive data visualizations in web browsers
+
+D3 is embedded within an HTML webpage, the javascript D3.js library uses pre-built javascript functions to select elements, create SVG(Scalable Vector Graphics) objects, style them or add transition, or dynamic effects
+
+The objects can be style using CSS
+
+Data visualization is a process of mapping data to visuals. Data in, visual properties out. Maybe bigger numbers make taller bars, or special categories trigger brighter colors. The mapping rules are up to you.
+
+With D3, we bind our data input values to elements in the DOM. Binding is like “attaching” or associating data to specific elements, so that later you can reference those values to apply mapping rules. Without the binding step, we have a bunch of data-less, un-mappable DOM elements. No one wants that.
---
+++ weather.js
@@ -0,0 +1,61 @@
+//temperature forecast
+var dataset = [ 61, 77, 66, 84, 66, 87, 67, 86 ];
+
+
+//Width and height
+var w = 600;
+var h = 400;
+var barPadding = 1;
+
+
+//append svg to the body with attributes of width = w and height = h
+
+//tell D3 to create an empty SVG element and add it to the DOM:
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+//generate rectangles and add them to svg
+svg.selectAll("rect")
+   .data(dataset)
+   .enter()
+   .append("rect")
+   .attr("y", function(d) {
+    return h-(d*4);  //Height minus data value
+    })
+   .attr("width", w / dataset.length - barPadding)
+   .attr("height", function(d) {
+    return d*4;  //Just the data value
+    })
+   .attr("x", function(d, i) {
+    return i * (w / dataset.length);
+    })
+    .attr("fill", function(d,i) {
+        if (d>70){
+            return "rgb(" + (d) + ", 0, 0 )";
+        }else{
+         return "rgb(0,0," + d +")" ;
+        }
+    });
+
+svg.selectAll("text")
+   .data(dataset)
+   .enter()
+   .append("text")
+   .text(function(d,i) {
+       if(i%2 !== 0){
+        return 'High: '+ d + '°F';
+       }else{
+           return 'Low: '+ d + '°F';
+       }
+   })
+   .attr("x", function(d, i) {
+        return i * (w / dataset.length) + 5;
+   })
+   .attr("y", function(d) {
+        return h - (d * 4) + 15; 
+   })
+   .attr("font-family", "sans-serif")
+   .attr("font-size", "11px")
+   .attr("fill", "white");
